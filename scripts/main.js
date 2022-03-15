b575ab30462c12ca8f11cdda00f93236d2b36395
judah <87juda@gmail.com>
2022-03-15 14:13:35 -0300

    Fix linter typos

--- scripts/main.js
+++ scripts/main.js
@@ -21,65 +21,9 @@ let bookshelf = new StrShelf();
 
 // Event Listeners & Logic
 
-function clearFrontDesk() {
-  // clear the slot that contains new added books from form
-  frontDesk = [];
-}
-
-function clearShelf() {
-  // Clear shelf array (id, title, author)
-  shelf = [];
-}
-
-function clearFrontShelf() {
-  // Clear html shelf (index.html)
-  frontShelf = [];
-}
-
 function pushToStorage(obj) {
   const stringify = JSON.stringify(obj);
-  //change this line below to localStorage.setItem('shelf', stringify);
   localStorage.setItem('strShelf', stringify);
-}
-
-function pullFromStorage() {
-  let parsed = JSON.parse(localStorage.getItem('strShelf'));
-  let counter = 1;
-  const preShelf = [];
-  for(let i = 0; i < shelf.length; i += 1){
-    let parsedBook = parsed[`${counter}`];
-    counter += 1;
-    preShelf.unshift(displayBook(parsedBook.title, parsedBook.author));
-  }
-  const createdBook = document.createElement('div');
-  createdBook.classList.add('book');
-  for(i = 0; i < preShelf.length; i += 1){
-    createdBook.innerHTML = preShelf[i];
-    frontShelf.prepend(createdBook);
-  }
-}
-
-function updateShelf () {
-  bookshelf = new StrShelf();
-  let counter = 0;
-  for(let i = 0; i < shelf.length; i += 1) {
-    counter += 1;
-    shelf[i].id = counter;
-  }
-  for (let i = 0; i < shelf.length; i += 1) {
-    const id = shelf[i].id;
-    bookshelf[`${id}`] = shelf[i];
-  }
-  pushToStorage(bookshelf);
-}
-
-function newBook(title, author) {
-  bookshelf = new StrShelf();
-  const book = new Book();
-  book.title = title;
-  book.author = author;
-  shelf.unshift(book);
-  updateShelf();
 }
 
 function displayBook(title, author) {
@@ -91,6 +35,46 @@ function displayBook(title, author) {
       <button class="removeButton" type="button">remove</button>
       <hr>
       `;
+}
+
+function pullFromStorage() {
+  const parsed = JSON.parse(localStorage.getItem('strShelf'));
+  let counter = 1;
+  const preShelf = [];
+  for (let i = 0; i < shelf.length; i += 1) {
+    const parsedBook = parsed[`${counter}`];
+    counter += 1;
+    preShelf.unshift(displayBook(parsedBook.title, parsedBook.author));
+  }
+  const createdBook = document.createElement('div');
+  createdBook.classList.add('book');
+  for (let i = 0; i < preShelf.length; i += 1) {
+    createdBook.innerHTML = preShelf[i];
+    frontShelf.prepend(createdBook);
+  }
+}
+
+function updateShelf() {
+  bookshelf = new StrShelf();
+  let counter = 0;
+  for (let i = 0; i < shelf.length; i += 1) {
+    counter += 1;
+    shelf[i].id = counter;
+  }
+  for (let i = 0; i < shelf.length; i += 1) {
+    const id = shelf[i].id;
+    bookshelf['${id}'] = shelf[i];
+  }
+  pushToStorage(bookshelf);
+}
+
+function newBook(title, author) {
+  bookshelf = new StrShelf();
+  const book = new Book();
+  book.title = title;
+  book.author = author;
+  shelf.unshift(book);
+  updateShelf();
 }
 
 addButton.addEventListener('click', (e) => {
