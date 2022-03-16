82004aee26c84356efadf1b6fb887a831f46518f
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-03-16 15:03:30 -0400

    Add JS for SPA design

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,45 @@
 /* eslint-disable max-classes-per-file */
 
-const homeBtn = getElementById('HomeBtn');
-const libraryBtn = getElementById('LibraryBtn');
-const newBtn = getElementById('NewBtn');
-const contactBtn = getElementById('#ContactBtn');
+const libraryBtn = document.getElementById('LibraryBtn');
+const newBtn = document.getElementById('NewBtn');
+const contactBtn = document.getElementById('ContactBtn');
 
-const libraryPage = getElementById('Library');
-const newPage = getElementById('New');
-const contactPage = getElementById('#Contact');
+const libraryPage = document.getElementById('Library');
+const newPage = document.getElementById('New');
+const contactPage = document.getElementById('Contact');
+
+const libraryStr = 'library';
+const hidden = 'hidden';
+const newStr = 'new';
+const contactStr = 'contact';
+
+function pageSetup() {
+  newPage.className = hidden;
+  contactPage.className = hidden;
+}
+
+pageSetup();
 
 contactBtn.addEventListener('click', () => {
-  libraryPage.classList.add('.hidden');
-  newPage.classList.add('.hidden');
-  contactPage.classList.add('.hidden');
+  libraryPage.className = hidden;
+  newPage.className = hidden;
+  contactPage.className = contactStr;
 });
 
-var dt = new Date();
-document.getElementById('date-time').innerHTML=dt;
+newBtn.addEventListener('click', () => {
+  libraryPage.className = hidden;
+  contactPage.className = hidden;
+  newPage.className = newStr;
+});
+
+libraryBtn.addEventListener('click', () => {
+  libraryPage.className = libraryStr;
+  newPage.className = hidden;
+  contactPage.className = hidden;
+});
+
+const dt = new Date();
+document.getElementById('date-time').innerHTML = dt;
 
 class Book {
   title;
